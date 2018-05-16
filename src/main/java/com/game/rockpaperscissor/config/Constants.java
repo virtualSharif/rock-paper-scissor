9bbadabbf1beabf1295adb29c2574e9e5f309fbc
sharif.malik <malik_sharif@ymail.com>
2018-05-16 09:27:28 +0200

    introduce constants to fetch name anytime

---
+++ src/main/java/com/game/rockpaperscissor/config/Constants.java
@@ -0,0 +1,9 @@
+package com.game.rockpaperscissor.config;
+
+public class Constants {
+
+    public static String PLAYER_TWO_NAME = "Kelvin (Computer)";
+
+    public static Integer WINNING_SCORE = 3;
+
+}
